16e20418c6b0a6fb280325e8c552ca5cd9305bdc
Chely Ho <adept_inept@yahoo.com>
2018-03-03 07:14:27 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -70,6 +70,9 @@ class Pokemon    #bonus version
  def alter_hp
 
  end
-  # The find method should create a new Pokemon object with an id, type, name AND hp after selecting their row from the database by their id number.
-  # remember to also update the initialize method to accept an argument of hp that defaults to nil if not set (so it still passes the non-bonus tests)
+ # So Ian and you have decided to battle.  He chose Magikarp (rookie mistake), and you chose Pikachu.
+ # He used splash. It wasn't very effective. It did one damage.
+ it "alters Pikachu's hp to 59" do
+   pikachu.alter_hp(59, @db)
+   expect(Pokemon.find(1, @db).hp).to eq(59)
 end
