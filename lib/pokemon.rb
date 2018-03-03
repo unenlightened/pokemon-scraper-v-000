0df1e1001158818133178f70ab1c606a625dca05
Chely Ho <adept_inept@yahoo.com>
2018-03-03 07:05:59 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,40 +1,40 @@
-require 'pry'
+#require 'pry'
+#
+#class Pokemonx    #orignal class that passes the first tests
+#  attr_accessor :name, :type, :db
+#  attr_reader :id
+#
+#  def initialize(name:, type:, db:, id: nil)
+#    @id = id
+#    @name = name
+#    @type = type
+#    @db = db
+#  end
+#
+#  def self.save(name, type, db)
+#    sql = <<-SQL
+#      INSERT INTO pokemon (name, type)
+#      VALUES (?, ?)
+#    SQL
+#
+#    db.execute(sql, name, type)
+#    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+#  end
+#
+#  def self.find(id, db)
+#    sql = <<-SQL
+#      SELECT *
+#      FROM pokemon
+#      WHERE ID = ?
+#    SQL
+#
+#    id, name, type = db.execute(sql, id.to_s).flatten
+#    self.new(id: id, name: name, type: type, db: db)
+#  end
+#
+#end
 
-class Pokemonx    #orignal class that passes the first tests
-  attr_accessor :name, :type, :db
-  attr_reader :id
-
-  def initialize(name:, type:, db:, id: nil)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
-  end
-
-  def self.save(name, type, db)
-    sql = <<-SQL
-      INSERT INTO pokemon (name, type)
-      VALUES (?, ?)
-    SQL
-
-    db.execute(sql, name, type)
-    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
-  end
-
-  def self.find(id, db)
-    sql = <<-SQL
-      SELECT *
-      FROM pokemon
-      WHERE ID = ?
-    SQL
-
-    id, name, type = db.execute(sql, id.to_s).flatten
-    self.new(id: id, name: name, type: type, db: db)
-  end
-
-end
-
-class Pokemon    #bonus version
+class Pokemon    bonus version
   attr_accessor :name, :type, :db, :hp
   attr_reader :id
 
